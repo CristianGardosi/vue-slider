906bc421b496d22337a95303d3b58e66cad6406d
Cristian Gardosi <cristian.gardosi97@gmail.com>
2020-11-09 14:12:06 +0100

    START

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// *****************************************************
+// SLIDER IMMAGINI GESTITO CON VUE
+// Gestire navigazione immagini con click su freccie pallini e freccette sx / dx tastiera
+// Fare eseguire il loop delle immagini, cambiando immagine ogni 3 secondi
+// *****************************************************
